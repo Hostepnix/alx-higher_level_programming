fbe02e1404e181234d26177cf3d863550e1ae46d
Hostepnix <stephane4love@gmail.com>
2022-09-16 15:50:59 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+Square = __import__('1-square').Square
 
-class Square:
-    def __init__(self, size=0):
-        self.__size = size
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
